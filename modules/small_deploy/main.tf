3892350611158a1d869b1d863114d35801691f8a
Chen Qingmin <chenqingmin@Chens-MacBook-Pro.local>
2020-08-26 15:34:58 +0800

    test azure pipeline API rest request

--- modules/small_deploy/main.tf
+++ modules/small_deploy/main.tf
@@ -15,7 +15,7 @@ resource "google_storage_bucket" "create-source-code-bucket" {
 }
 
 resource "google_storage_bucket" "restore_state_bucket" {
-  name = "terraform_state_colletion"
+  name = "terraform_state_colletion_small"
   location = "us-central1"
   project = "dataprod-cluster-testing-1"
   storage_class = "STANDARD"
@@ -24,15 +24,15 @@ resource "google_storage_bucket" "restore_state_bucket" {
 }
 
 //the following way couldn't upload the completed state file, it doesn't the case we need
-resource "google_storage_bucket_object" "upload-state-file" {     //used
-  name   = "terraform_state_restore/"
+resource "google_storage_bucket_object" "upload-code-file" {     //used
+  name   = "codes/"
   content = "Not really a directory, but it's empty"
-  bucket = trimprefix(var.root-small-deploy-source-code-bucket,"gs://")
-  //bucket = "gs://integrate-source-code-small-for-testing"
+  bucket = trimprefix("${var.root-small-deploy-source-code-bucket}","gs://")
+  //bucket = "integrate-source-code-small-for-testing"
   content_type = "text/x-sh"
   provisioner "local-exec" {
-    //command = "gsutil cp -r /Users/chenqingmin/Codes/terraform_test_project_temp/AllServicesKey.json ${var.root-scala-code-jar-bucket}/security"        //used
-    command = "gsutil cp -r ${var.root-terraform-project-home-folder}/main.tf ${var.root-small-deploy-source-code-bucket}/terraform_state_restore"
+    command = "gsutil cp -r /Users/chenqingmin/Codes/terraform_test_project_temp_small_2/main.tf ${var.root-small-deploy-source-code-bucket}/codes/"        //used
+    //command = "gsutil cp -r ${var.root-terraform-project-home-folder}/main.tf ${var.root-small-deploy-source-code-bucket}/codes/"
   }
   depends_on = [null_resource.auth_gcloud]
 }
@@ -40,6 +40,7 @@ resource "google_storage_bucket_object" "upload-state-file" {     //used
 resource "null_resource" "auth_gcloud" {   //used
   provisioner "local-exec" {
     command = "gcloud auth activate-service-account 742690957765-compute@developer.gserviceaccount.com --key-file=${var.root-terraform-project-home-folder}/dataprod-cluster-testing-1-7444c4c90649.json --project=dataprod-cluster-testing-1"
+    //command = "gcloud auth activate-service-account 742690957765-compute@developer.gserviceaccount.com --key-file=/Users/chenqingmin/Codes/terraform_test_project_temp_small_2/dataprod-cluster-testing-1-7444c4c90649.json --project=dataprod-cluster-testing-1"
   }
   depends_on = [google_storage_bucket.create-source-code-bucket]
 }
