6338e031537bd8903ad5b8ff3238303220bbe4eb
Chen Qingmin <chenqingmin@Chens-MacBook-Pro.local>
2020-09-07 18:18:57 +0800

    plugin can't be found in azure pipeline extenstion

--- modules/notify_destroy/main.tf
+++ modules/notify_destroy/main.tf
@@ -5,6 +5,21 @@ provider "google" {
   credentials = file("dataprod-cluster-testing-1-7444c4c90649.json")
 }
 
+terraform {
+    required_providers {
+        # HashiCorp's dns provider
+        google = {
+            source = "hashicorp/google"
+            version = "~> 3.37.0"
+        }
+        # A hypothetical alternative dns provider
+        mydns = {
+            source = "hashicorp/null"
+            version = "~> 2.1.2"
+        }
+    }
+}
+
 data "google_composer_image_versions" "all" {
 }
 
