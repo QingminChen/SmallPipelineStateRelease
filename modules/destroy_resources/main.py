228be1af70e01bd0824d0c118606c231f4fcec75
Chen Qingmin <chenqingmin@Chens-MacBook-Pro.local>
2020-09-07 13:44:11 +0800

    add the destroy part

---
+++ modules/destroy_resources/main.py
@@ -0,0 +1,35 @@
+import base64
+import json
+import base64
+import requests
+import logging
+import string
+import base64
+import fileinput
+import logging
+from datetime import datetime
+from io import BytesIO
+from azure.devops.connection import Connection
+from msrest.authentication import BasicAuthentication
+import azure
+import pprint
+import datetime
+import pytz
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+def notify_topic_subscriber(event, context):
+    """Triggered from a message on a Cloud Pub/Sub topic.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    if pubsub_message=='ok':
+         personal_access_token = 'kkrubziptxnvmkhxo7i5m5qjs6744i6kh6gotf73ksumbz4v7waq'
+         organization_url = 'https://dev.azure.com/testinggcpuser'
+         personal_access_token_bytes_url = personal_access_token.encode("ascii")
+         credentials = BasicAuthentication('', personal_access_token)
+         connection = Connection(base_url=organization_url, creds=credentials)
+         release_client = connection.clients_v5_1.get_release_client()
+         update_release_environment_response = release_client.update_release_environment(azure.devops.v5_1.release.models.ReleaseEnvironmentUpdateMetadata(comment="Test trigger API",status='inProgress'),'PipelineIntegrateWithGCPDeployment', 56, 95)
--- modules/small_deploy/main.tf
+++ modules/small_deploy/main.tf
@@ -31,8 +31,15 @@ resource "google_storage_bucket_object" "upload-code-file" {     //used
   //bucket = "integrate-source-code-small-for-testing"
   content_type = "text/x-sh"
   provisioner "local-exec" {
-    command = "gsutil cp -r /Users/chenqingmin/Codes/terraform_test_project_temp_small_2/main.tf ${var.root-small-deploy-source-code-bucket}/codes/"        //used
+    command = "gsutil cp -r /Users/chenqingmin/Codes/Github_Tools_Workspace/SmallPipelineStateRelease/destory.zip ${var.root-small-deploy-source-code-bucket}/codes/"        //used
     //command = "gsutil cp -r ${var.root-terraform-project-home-folder}/main.tf ${var.root-small-deploy-source-code-bucket}/codes/"
+  }
+  depends_on = [null_resource.auth_gcloud]
+}
+
+resource "null_resource" "pakcgae-destory-zip-file" {     //used
+  provisioner "local-exec" {
+    command = "zip -j destory.zip ./modules/destroy_resources/*"
   }
   depends_on = [null_resource.auth_gcloud]
 }
@@ -43,5 +50,43 @@ resource "null_resource" "auth_gcloud" {   //used
     //command = "gcloud auth activate-service-account 742690957765-compute@developer.gserviceaccount.com --key-file=/Users/chenqingmin/Codes/terraform_test_project_temp_small_2/dataprod-cluster-testing-1-7444c4c90649.json --project=dataprod-cluster-testing-1"
   }
   depends_on = [google_storage_bucket.create-source-code-bucket]
+}
+
+
+resource "google_cloudfunctions_function" "create_deploy_cloud_function" {
+  name                  = "cloud-function-test-1"
+  project               = "dataprod-cluster-testing-1"
+  description           = "Trigger for destroying"
+  available_memory_mb   = 128
+  source_archive_bucket = trimprefix("${var.root-small-deploy-source-code-bucket}","gs://")
+  source_archive_object = "codes/destory.zip"
+  timeout               = 300
+  entry_point           = "notify_topic_subscriber"
+  //trigger_http          = false
+  runtime               = "python37"
+  event_trigger         {
+      event_type            ="google.pubsub.topic.publish"
+      resource              ="projects/dataprod-cluster-testing-1/topics/notify_destory_topic"
+  }
+//  source_repository     {
+//      url              ="../destroy_resources"
+//  }
+
+  service_account_email = "742690957765-compute@developer.gserviceaccount.com"
+
+  depends_on = [google_pubsub_topic.create_topic]
+
+}
+
+resource "google_pubsub_topic" "create_topic" {
+  name = "notify_destory_topic"
+  project = "dataprod-cluster-testing-1"
+
+  message_storage_policy {
+    allowed_persistence_regions = [
+      "us-central1"
+    ]
+  }
+  depends_on = [null_resource.auth_gcloud]
 }
 
